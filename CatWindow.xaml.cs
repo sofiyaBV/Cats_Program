c7272a35dc2df56b9fa6874368d033bbebbb3478
Bond_vb28 <Bond_vb28@student.itstep.org>
2023-10-14 19:52:32 +0300

    Создание визуала

---
+++ CatWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Cats_Program
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class CatWindow : Window
+    {
+        public CatWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
