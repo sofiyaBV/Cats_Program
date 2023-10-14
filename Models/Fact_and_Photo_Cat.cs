485b5f3798fd06946c414eea22be638510f41113
Bond_vb28 <Bond_vb28@student.itstep.org>
2023-10-14 22:48:16 +0300

    загрузка фотографий и фактов (10 штук)

---
+++ Models/Fact_and_Photo_Cat.cs
@@ -0,0 +1,21 @@
+﻿using Cats_Program.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cats_Program.Models
+{
+    public class Fact_and_Photo_Cat
+    {
+        public Cat_Photo photo { get; set; }
+        public string factsCat { get; set; }
+
+        public Fact_and_Photo_Cat(Cat_Photo photo, string factsCat)
+        {
+            this.photo = photo;
+            this.factsCat = factsCat;
+        }
+    }
+}
